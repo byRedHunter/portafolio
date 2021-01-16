df80a73ba3e345ae501353f7881f5ceff297fde8
Jhonny Quispe Navarro <jhonny.quispejl@gmail.com>
2021-01-15 21:50:28 -0500

    cambiamos las imagenes de los videos de youtube

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,34 @@
+/* toggle menu */
 const $header = document.querySelector('#header')
 const $menuIcon = document.querySelector('#menu-icon')
 
 $menuIcon.addEventListener('click', () => {
 	$header.classList.toggle('active')
+})
+
+/* ocultar menu al dar clic a los item del menu */
+const $enlaces = document.querySelectorAll('.enlace-menu')
+
+const hideMenu = () => {
+	$header.classList.remove('active')
+}
+
+$enlaces.forEach((enlace) => {
+	enlace.addEventListener('click', hideMenu)
+})
+
+/* toggle portfolio */
+const $textFilters = document.querySelectorAll('.filtro__container--item')
+
+const toggleFilter = (textFilter) => {
+	document
+		.querySelector('.filtro__container--item.active')
+		.classList.remove('active')
+	textFilter.classList.add('active')
+}
+
+$textFilters.forEach((textFilter) => {
+	textFilter.addEventListener('click', () => {
+		toggleFilter(textFilter)
+	})
 })
